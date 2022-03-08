62c5b1942fce47f408f8531c0f615d2608607341
JDetwilerITCS <97992813+JDetwilerITCS@users.noreply.github.com>
2022-03-08 14:33:34 -0800

    v
    e

---
+++ Final project GD2/Assets/Scripts/TileController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public float MoveX;
+    public float MXtime;
+    public float MoveY;
+    public float MYtime;
+    public float MoveZ;
+    public float MZtime;
+    public float RotateX;
+    public float RotateY;
+    public float RotateZ;
+    public float ScaleX;
+    public float ScaleY;
+    public float ScaleZ;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * MoveX);
+        transform.Translate(Vector3.up * Time.deltaTime * MoveY);
+        transform.Translate(Vector3.forward * Time.deltaTime * MoveZ);
+        transform.Rotate(Vector3.right * Time.deltaTime * RotateX);
+        transform.Rotate(Vector3.up * Time.deltaTime * RotateY);
+        transform.Rotate(Vector3.forward * Time.deltaTime * RotateZ);
+        transform.Rotate(Vector3.forward * Time.deltaTime * ScaleX);
+        transform.Rotate(Vector3.forward * Time.deltaTime * ScaleY);
+        transform.Rotate(Vector3.forward * Time.deltaTime * ScaleZ);
+    }
+}
