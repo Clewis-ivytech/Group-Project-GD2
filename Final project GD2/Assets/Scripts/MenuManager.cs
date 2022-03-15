d60d875ef839b3e3869041dccc0979e4828ff37d
KittensWithTinyMittens <98059607+KittensWithTinyMittens@users.noreply.github.com>
2022-03-15 14:29:25 -0700

    Level chooser and Remmy Script
    Aahahahahahahahahahaha

--- Final project GD2/Assets/Scripts/MenuManager.cs
+++ Final project GD2/Assets/Scripts/MenuManager.cs
@@ -7,12 +7,47 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     //
-
+    public GameObject MainMenuPanal;
+    public GameObject LevelPanal;
     //
 
-    public void StartGame()
+    public void LoadLevelOne()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void LoadLevelTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevelThree()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadLevelFour()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadLevelFive()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void LevelPanalOn()
+    {
+        LevelPanal.SetActive(true);
+        MainMenuPanal.SetActive(false);
+    }
+
+    public void LevelPanalOff()
+    {
+        MainMenuPanal.SetActive(true);
+        LevelPanal.SetActive(false);
+    }
+
+
 
 }
--- Final project GD2/Assets/Scripts/RemmyCoin.cs
+++ Final project GD2/Assets/Scripts/RemmyCoin.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class RemmyCoin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //
+    public int ID;
+    public int LevelID;
+    public bool Collected = false;
+    public RemmyCoin Instance;
+    public GameObject remmyCoin;
+    public Rigidbody remmyCoinRB;
+    public LevelUIManager LevelUIManager;
+    //
+    private void Awake()
     {
-        
+        Collected = false;
+    }
+    private void Update()
+    {
+        if(Collected)
+        {
+           remmyCoin.SetActive(false);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.CompareTag("Character"))
+        {
+            LevelUIManager.RemmyCoinCollected = LevelUIManager.RemmyCoinCollected + 1;
+            Collected = true;
+        }
     }
+
 }
