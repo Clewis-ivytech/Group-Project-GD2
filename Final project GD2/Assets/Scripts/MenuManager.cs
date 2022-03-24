31c9610ab5474c930a2738aec6c95eeb050e788e
JDetwilerITCS <97992813+JDetwilerITCS@users.noreply.github.com>
2022-03-24 14:32:05 -0700

    char

--- Final project GD2/Assets/Scripts/MenuManager.cs
+++ Final project GD2/Assets/Scripts/MenuManager.cs
@@ -11,6 +11,37 @@ public class MenuManager : MonoBehaviour
     public GameObject SettingsPanal;
     public GameObject CharacterPanal;
 
+    public GameObject Character1;
+    public GameObject Character2;
+    public GameObject Character3;
+    public float CurrentCharacter;
+    public bool Char2Lock;
+    public bool Char3Lock;
+    public TMP_Text Char2Txt;
+    public TMP_Text Char3Txt;
+
+    private void Start()
+    {
+        CurrentCharacter = 1;
+        if (Char2Lock == true)
+        {
+            Char2Txt.SetText("PlaceHolder");
+        }
+        else
+        {
+            Char2Txt.SetText("(Locked)");
+        }
+
+        if (Char3Lock == true)
+        {
+            Char3Txt.SetText("PlaceHolder");
+        }
+        else
+        {
+            Char3Txt.SetText("(Locked)");
+        }
+    }
+
     public void LoadLevelOne()
     {
         SceneManager.LoadScene(1);
@@ -68,8 +99,45 @@ public class MenuManager : MonoBehaviour
 
     public void CharacterPanelOff()
     {
-        CharacterPanal.SetActive(true);
-        SettingsPanal.SetActive(false);
+        CharacterPanal.SetActive(false);
+        MainMenuPanal.SetActive(true);
     }
 
+    public void CharacterOne()
+    {
+        Character1.SetActive(true);
+        Character2.SetActive(false);
+        Character3.SetActive(false);
+        CurrentCharacter = 1;
+    }
+
+    public void CharacterTwo()
+    {
+        if (Char2Lock == true)
+        {
+            Character1.SetActive(false);
+            Character2.SetActive(true);
+            Character3.SetActive(false);
+            CurrentCharacter = 2;
+        }
+        else
+        {
+            Char2Txt.SetText("(Locked)");
+        }
+    }
+
+    public void CharacterThree()
+    {
+        if (Char3Lock == true)
+        {
+            Character1.SetActive(false);
+            Character2.SetActive(false);
+            Character3.SetActive(true);
+            CurrentCharacter = 3;
+        }
+        else
+        {
+            Char3Txt.SetText("(Locked)");
+        }
+    }
 }
